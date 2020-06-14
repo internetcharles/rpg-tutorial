93992809b9596ef57085493a61da887b1753235f
internetcharles <charlesodellsmith@gmail.com>
2020-06-14 13:30:54 -0700

    CHANGES attacker

--- Assets/Script/Control/PlayerController.cs
+++ Assets/Script/Control/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using RPG.Combat;
 using RPG.Movement;
 using UnityEngine;
 
@@ -7,6 +8,27 @@ public class PlayerController : MonoBehaviour
 {
 
     void Update()
+    {
+        InteractWithCombat();
+        InteractWithMovement();
+    }
+
+    private void InteractWithCombat()
+    {
+        RaycastHit[] hits = Physics.RaycastAll(GetRay());
+        foreach (var hit in hits)
+        {
+            CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+            if (target == null) continue;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Fighter>().Attack(target);
+            }
+        }
+    }
+
+    public void InteractWithMovement()
     {
         if (Input.GetMouseButton(0))
         {
@@ -16,13 +38,18 @@ public class PlayerController : MonoBehaviour
 
     private void MoveToCursor()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = GetRay();
         RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
+        bool hasHit = Physics.Raycast(GetRay(), out hit);
         if (hasHit)
         {
             GetComponent<Mover>().MoveTo(hit.point);
         }
+    }
+
+    private static Ray GetRay()
+    {
+        return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 
 }
