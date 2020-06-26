c8d499c3827f2ec535b258537cd84a6e9c24d85a
internetcharles <charlesodellsmith@gmail.com>
2020-06-26 12:25:44 -0700

    FIXED

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Combat;
+using RPG.Core;
+using UnityEngine;
+using Health = RPG.Core.Health;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        Fighter fighter;
+        Health health;
+        GameObject player;
+
+        private void Start() {
+            fighter = GetComponent<Fighter>();
+            health = GetComponent<Health>();
+            player = GameObject.FindWithTag("Player");
+        }
+
+        private void Update()
+        {
+            if (health.IsDead()) return;
+
+            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
+            {
+                fighter.Attack(player);
+            }
+            else
+            {
+                fighter.Cancel();
+            }
+        }
+
+        private bool InAttackRangeOfPlayer()
+        {
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            return distanceToPlayer < chaseDistance;
+        }
+    }
+}
